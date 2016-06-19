5564a3f15be6aa73e7ea34b514ba25cb31444433
floriankammermann <florian@kammermann.me>
2016-06-19 14:11:24 +0200

    changed response message

--- hello-world-java/src/main/java/org/swisspush/vertx/examples/HelloJugsBern.java
+++ hello-world-java/src/main/java/org/swisspush/vertx/examples/HelloJugsBern.java
@@ -7,7 +7,7 @@ public class HelloJugsBern extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(request -> {
-            request.response().end("Hello Jugs Bern world !");
+            request.response().end("Hello Jugs Bern!");
         }).listen(8080);
     }
 }
