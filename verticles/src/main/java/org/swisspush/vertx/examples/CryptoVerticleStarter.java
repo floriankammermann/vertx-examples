8199f167979cf694d7c5b5527f7a2f38b0428b48
floriankammermann <florian@kammermann.me>
2016-06-21 14:12:16 +0200

    added sys property for instance amount

--- verticles/src/main/java/org/swisspush/vertx/examples/CryptoVerticleStarter.java
+++ verticles/src/main/java/org/swisspush/vertx/examples/CryptoVerticleStarter.java
@@ -8,8 +8,14 @@ public class CryptoVerticleStarter {
 
     public static void main(String[] args) {
 
+        int cryptoInstances = 1;
+        try {
+            cryptoInstances = Integer.parseInt(System.getProperty("cryptoInstances"));
+        } catch (Exception e) {
+            // do nothing, take default instances
+        }
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle("org.swisspush.vertx.examples.CryptoVerticle", new DeploymentOptions().setInstances(1), eventCrypto -> {
+        vertx.deployVerticle("org.swisspush.vertx.examples.CryptoVerticle", new DeploymentOptions().setInstances(cryptoInstances), eventCrypto -> {
             LoggerFactory.getLogger(CryptoVerticleStarter.class).info("deployed Crypto Verticles: " + eventCrypto.toString());
             vertx.deployVerticle("org.swisspush.vertx.examples.CounterVerticle", new DeploymentOptions().setInstances(1), eventCounter -> {
                 LoggerFactory.getLogger(CryptoVerticleStarter.class).info("deployed Counter Verticles: " + eventCounter.toString());
