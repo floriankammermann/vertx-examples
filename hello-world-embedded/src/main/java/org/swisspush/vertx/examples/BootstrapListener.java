7a3579a508c03979f65df34c4f3334c00a4dfb15
floriankammermann <florian@kammermann.me>
2016-06-21 12:23:18 +0200

    added hello world embedded

---
+++ hello-world-embedded/src/main/java/org/swisspush/vertx/examples/BootstrapListener.java
@@ -0,0 +1,40 @@
+/*
+ * ------------------------------------------------------------------------------------------------
+ * Copyright 2013 by Swiss Post, Information Technology Services
+ * ------------------------------------------------------------------------------------------------
+ * $Id$
+ * ------------------------------------------------------------------------------------------------
+ */
+package org.swisspush.vertx.examples;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class BootstrapListener implements ServletContextListener {
+
+    private static final Logger log = LoggerFactory.getLogger(BootstrapListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent context) {
+
+        Vertx.vertx().deployVerticle("org.swisspush.vertx.examples.HelloJugsBern", result -> {
+            if (result.succeeded()) {
+                String deploymentID = result.result();
+                log.debug("Module has been deployed with deploymentID {}", deploymentID);
+            } else {
+                throw new RuntimeException("Module not started: {}", result.cause());
+            }
+        });
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+
+}
---
+++ hello-world-embedded/src/main/java/org/swisspush/vertx/examples/HelloJugsBern.java
@@ -0,0 +1,13 @@
+package org.swisspush.vertx.examples;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloJugsBern extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer().requestHandler(request -> {
+            request.response().end("Hello Jugs Bern!");
+        }).listen(8989);
+    }
+}
