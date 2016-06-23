14ab99332bbdbf67a32ffb927fa0418b0288a103
floriankammermann <florian@kammermann.me>
2016-06-23 14:19:52 +0200

    changed to jug

--- hello-world-java/src/main/java/org/swisspush/vertx/examples/HelloJugBern.java
+++ hello-world-java/src/main/java/org/swisspush/vertx/examples/HelloJugBern.java
@@ -2,14 +2,14 @@ package org.swisspush.vertx.examples;
 
 import io.vertx.core.AbstractVerticle;
 
-public class HelloJugsBern extends AbstractVerticle {
+public class HelloJugBern extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(request -> {
             request.response()
                     .putHeader("content-type", "application/json; charset=utf-8")
-                    .end("{\"greeting\":\"Hello Jugs Bern!\", \"thread\":\""+Thread.currentThread().getName()+"\"}");
+                    .end("{\"greeting\":\"Hello Jug Bern!\", \"thread\":\""+Thread.currentThread().getName()+"\"}");
         }).listen(8080);
     }
 }
