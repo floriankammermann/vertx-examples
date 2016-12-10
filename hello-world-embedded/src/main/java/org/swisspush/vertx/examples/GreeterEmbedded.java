a1be518c10486682743e79824de937b6ac0b03fb
floriankammermann <florian@kammermann.me>
2016-12-10 06:48:22 +0100

    removed jug bern from names

--- hello-world-embedded/src/main/java/org/swisspush/vertx/examples/GreeterEmbedded.java
+++ hello-world-embedded/src/main/java/org/swisspush/vertx/examples/GreeterEmbedded.java
@@ -2,12 +2,12 @@ package org.swisspush.vertx.examples;
 
 import io.vertx.core.AbstractVerticle;
 
-public class HelloJugBern extends AbstractVerticle {
+public class GreeterEmbedded extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(request -> {
-            request.response().end("Hello Jug Bern, I run in a tomcat\n\n\n" +
+            request.response().end("Hello Impacthub, I run in a tomcat\n\n\n" +
                                    "Here is my environment:\n"+System.getProperties());
         }).listen(8989);
     }
