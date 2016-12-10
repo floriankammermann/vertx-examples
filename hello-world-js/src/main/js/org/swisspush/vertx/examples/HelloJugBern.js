258eb627937ee4a0cd194553fb2ea12494a40364
floriankammermann <florian@kammermann.me>
2016-12-10 06:26:08 +0100

    removed jug bern from names

--- hello-world-js/src/main/js/org/swisspush/vertx/examples/HelloJugBern.js
+++
@@ -1,3 +0,0 @@
-vertx.createHttpServer().requestHandler(function (req) {
-  req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello Jug Bern</h1></body></html>");
-}).listen(8080);
