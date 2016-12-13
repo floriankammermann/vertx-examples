a9ab2d96f3937c4221f849b96f004354e765007f
floriankammermann <florian@kammermann.me>
2016-12-13 22:50:05 +0100

    added handling of server start failure

--- hello-world-java/src/test/java/org/swisspush/vertx/examples/GreeterTest.java
+++ hello-world-java/src/test/java/org/swisspush/vertx/examples/GreeterTest.java
@@ -8,6 +8,7 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -33,10 +34,16 @@ public class GreeterTest {
         RestAssured.defaultParser = Parser.JSON;
 
         Greeter greeter = new Greeter();
-        vertx.deployVerticle(greeter, context.asyncAssertSuccess(assertSuccess -> {
-            log.info("verticle deployed: " + assertSuccess);
-            when().get("/").then().assertThat().statusCode(200).body("greeting", equalTo("Hello Impacthub!"));
-            async.complete();
-        }));
+        vertx.deployVerticle(greeter, result -> {
+            if(result.succeeded()) {
+                log.info("verticle deployed: " + result.result());
+                when().get("/").then().assertThat().statusCode(200).body("greeting", equalTo("Hello Impacthub!"));
+                async.complete();
+            } else {
+                log.error("failed to deploy verticle: " + result.cause());
+                Assert.fail();
+                async.complete();
+            }
+        });
     }
 }
