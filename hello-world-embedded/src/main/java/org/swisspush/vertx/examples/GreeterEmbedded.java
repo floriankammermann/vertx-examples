90f346d2ce75fe7c46a878110f80579f9e367f2c
floriankammermann <florian@kammermann.me>
2016-12-13 22:48:39 +0100

    added handling of server start failure

--- hello-world-embedded/src/main/java/org/swisspush/vertx/examples/GreeterEmbedded.java
+++ hello-world-embedded/src/main/java/org/swisspush/vertx/examples/GreeterEmbedded.java
@@ -1,14 +1,21 @@
 package org.swisspush.vertx.examples;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class GreeterEmbedded extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
+    public void start(Future<Void> fut) throws Exception {
         vertx.createHttpServer().requestHandler(request -> {
             request.response().end("Hello Impacthub, I run in a tomcat\n\n\n" +
                                    "Here is my environment:\n"+System.getProperties());
-        }).listen(8989);
+        }).listen(8989, result -> {
+            if (result.succeeded()) {
+                fut.complete();
+            } else {
+                fut.fail(result.cause());
+            }
+        });
     }
 }
