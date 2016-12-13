6d04e7e4df986895f36f5c6716d58384c799603e
floriankammermann <florian@kammermann.me>
2016-12-13 22:49:22 +0100

    added handling of server start failure

--- hello-world-java/src/main/java/org/swisspush/vertx/examples/Greeter.java
+++ hello-world-java/src/main/java/org/swisspush/vertx/examples/Greeter.java
@@ -1,15 +1,22 @@
 package org.swisspush.vertx.examples;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class Greeter extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
+    public void start(Future<Void> fut) throws Exception {
         vertx.createHttpServer().requestHandler(request -> {
             request.response()
                     .putHeader("content-type", "application/json; charset=utf-8")
                     .end("{\"greeting\":\"Hello Impacthub!\", \"thread\":\""+Thread.currentThread().getName()+"\"}");
-        }).listen(8080);
+        }).listen(8080, result -> {
+            if (result.succeeded()) {
+                fut.complete();
+            } else {
+                fut.fail(result.cause());
+            }
+        });
     }
 }
